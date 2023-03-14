c69515d55f6ff82e81f78f8b584120320a6085e6
Samuel Odongo <samieodd@gmail.com>
2023-03-14 18:53:46 +0300

    Create functions to add todos to DOM

---
+++ index.js
@@ -0,0 +1,69 @@
+const titleTextBox = document.getElementById('title-textbox');
+const contentTextBox = document.getElementById('content-textbox');
+const errorParagraph = document.getElementById('error-txt');
+const todoListDiv = document.getElementById('todo-list');
+
+let todoItems = [];
+
+class TodoItem {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.isComplete = false;
+    }
+}
+
+function addNewTodo() {
+    if (titleTextBox.value !== '' && contentTextBox.value !== '') {
+        errorParagraph.innerHTML = '';
+        todoItems.push(
+            new TodoItem(titleTextBox.value, contentTextBox.value)
+        );
+        updateTodoListDOMElements();
+    } else {
+        errorParagraph.innerHTML = 'Title and description cannot be empty.';
+    }
+}
+
+function updateTodoListDOMElements() {
+    if(todoItems.length > 0) {
+        todoListDiv.innerHTML = '';
+        console.log(todoItems.length);
+        for (let index = 0; index < todoItems.length; index++) {
+            let item = todoItems[index];
+            let itemID = 'todo-' + index;
+            let todoContainer = document.createElement('div');
+            todoContainer.className = 'todo-item column';
+            todoContainer.id = itemID;
+
+            let todoTitle = document.createElement('h4');
+            todoTitle.className = 'title';
+            todoTitle.innerText = item.title;
+
+            let todoDescription = document.createElement('p');
+            todoDescription.className = 'content';
+            todoDescription.innerText = item.description;
+
+            let row = document.createElement('div');
+            row.className = 'row';
+
+            let completeButton = document.createElement('button');
+            completeButton.className = 'btn';
+            completeButton.textContent = 'Complete';
+            let editButton = document.createElement('button');
+            editButton.className = 'btn';
+            editButton.textContent = 'Edit';
+            let deleteButton = document.createElement('button');
+            deleteButton.className = 'btn';
+            deleteButton.textContent = 'Delete';
+
+            row.appendChild(completeButton);
+            row.appendChild(editButton);
+            row.appendChild(deleteButton);
+            todoContainer.appendChild(todoTitle);
+            todoContainer.appendChild(todoDescription);
+            todoContainer.appendChild(row);
+            todoListDiv.appendChild(todoContainer);
+        }
+    }
+}
